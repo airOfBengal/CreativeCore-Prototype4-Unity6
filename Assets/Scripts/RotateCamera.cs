7aad34b2a95d90c425b8704edc21a907b1bc5da6
Md. Atiqul Islam <air.cse.pbn@gmail.com>
2025-08-07 22:50:25 +0600

    add camera rotation around a focul point

---
+++ Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+    public float rotationSpeed = 10f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up, horizontalInput * rotationSpeed * Time.deltaTime);
+    }
+}
